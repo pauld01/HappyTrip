640e61246ad57fbc93b428a4193109e2a8fb447f
Yaami Tachi <nicolas.ferreira@efrei.net>
2023-12-09 15:22:58 +0100

    V1 module de paiement

---
+++ src/app/directives/expiracy-date.directive.ts
@@ -0,0 +1,19 @@
+import { Directive } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
+import { validateExpiryDate } from '../validators/card-validator';
+
+@Directive({
+  selector: '[appValidateExpiry]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: ExpiryDateDirective,
+      multi: true
+    }
+  ]
+})
+export class ExpiryDateDirective implements Validator {
+  validate(control: AbstractControl): {[key: string]: any} | null {
+    return validateExpiryDate(control.value) ? null : {'expiryDateInvalid': true};
+  }
+}
--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { validateCardNumber as cardNumberValidator, validateExpiryDate as expiryDateValidator } from '../validators/card-validator';
 
 @Component({
-  selector: 'app-reservation',
-  templateUrl: './reservation.component.html',
-  styleUrl: './reservation.component.scss'
+    selector: 'app-reservation',
+    templateUrl: './reservation.component.html',
+    styleUrls: ['./reservation.component.scss']
 })
 export class ReservationComponent {
+    cardNumber: string = '';
+    expiryDate: string = '';
+    cvv: string = '';
 
+    validateCardNumber(value: string): boolean {
+        return cardNumberValidator(value);
+    }
+
+    validateExpiryDate(value: string): boolean {
+        return expiryDateValidator(value);
+    }
+
+    submitReservation() {
+        const isCardNumberValid = this.validateCardNumber(this.cardNumber);
+        const isExpiryDateValid = this.validateExpiryDate(this.expiryDate);
+        // Since you want to remove CVV validation, we will not check it here
+        if (isCardNumberValid && isExpiryDateValid) {
+            console.log('Card details are valid. Proceeding with reservation.');
+            console.log(`Card Number: ${this.cardNumber}, Expiry Date: ${this.expiryDate}`);
+        } else {
+            console.error('Invalid card details.');
+            if (!isCardNumberValid) console.error('Invalid card number.');
+            if (!isExpiryDateValid) console.error('Invalid expiry date.');
+        }
+    }
 }
