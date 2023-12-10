7f098943f19f451be77d49dc81856caa81d4299e
vinccrn <v.cornee@orange.fr>
2023-12-10 19:47:22 +0100

    add conform homepage

--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
   homeImageUrl: string = 'assets/auto-home.svg';
+  isDisplayed : boolean = false;
+  isShowed : boolean = true
+  ages: number[] = [18, 19, 20, 21, 22, 23, 24];
+
+  ShowOrHide() {
+    this.isDisplayed = !this.isDisplayed;
+  }
+
+  ShowOrHideField() {
+    this.isShowed = !this.isShowed;
+  }
 
   onAutoButtonClick(): void {
     this.homeImageUrl = 'assets/auto-home.svg';
