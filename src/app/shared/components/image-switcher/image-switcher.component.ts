6bcbf923f8f41ea7000782c81b917c2f08e94cf3
Paul DUPLESSI <paul.duplessi@hotmail.fr>
2023-12-12 21:33:15 +0100

    add active

--- src/app/shared/components/image-switcher/image-switcher.component.ts
+++ src/app/shared/components/image-switcher/image-switcher.component.ts
@@ -1,9 +1,12 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-image-switcher',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './image-switcher.component.html',
   styleUrl: './image-switcher.component.scss'
 })
@@ -11,11 +14,15 @@ export class ImageSwitcherComponent {
   @Output() autoClicked: EventEmitter<void> = new EventEmitter<void>();
   @Output() utilitaireClicked: EventEmitter<void> = new EventEmitter<void>();
 
+  vehiculeType: number = 1;
+
   onAutoButtonClick(): void {
     this.autoClicked.emit();
+    this.vehiculeType = 1;
   }
 
   onUtilitaireButtonClick(): void {
     this.utilitaireClicked.emit();
+    this.vehiculeType = 2;
   }
 }
